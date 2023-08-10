3fdfd9992a2806d39a95da552b92d604a6b90f9a
cksajil <sajilck@gmail.com>
2023-08-10 22:05:53 +0530

    change frame size

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ ssl._create_default_https_context = ssl._create_stdlib_context
 
 config = load_config("my_config.yaml")
 
+color_mapping = [
+    [255, 0, 0],  # Class 0 - Red
+    [0, 255, 0],  # Class 1 - Green
+    [0, 0, 255],  # Class 2 - Blue
+    [255, 255, 0],  # Class 3 - Yellow
+    [0, 255, 255],  # Class 4 - Cyan
+    [255, 0, 255],  # Class 5 - Magenta
+    [128, 0, 0],  # Class 6 - Maroon
+    [0, 128, 0],  # Class 7 - Green (Dark)
+    [0, 0, 128],  # Class 8 - Navy
+    [128, 128, 0],  # Class 9 - Olive
+    [0, 128, 128],  # Class 10 - Teal
+    [128, 0, 128],  # Class 11 - Purple
+    [255, 165, 0],  # Class 12 - Orange
+    [210, 180, 140],  # Class 13 - Tan
+    [70, 130, 180],  # Class 14 - Steel Blue
+    [139, 69, 19],  # Class 15 - Saddle Brown
+    [0, 255, 127],  # Class 16 - Spring Green
+    [255, 255, 255],  # Class 17 - White
+    [0, 0, 0],  # Class 18 - Black
+    [128, 128, 128],  # Class 19 - Gray
+    [255, 255, 128],  # Class 20 - Light Yellow
+]
+
 
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
@@ -20,14 +44,12 @@ def on_progress(stream, chunk, bytes_remaining):
     print(f"Downloading: {percentage:.2f}%")
 
 
-def colorize_seg_image(frame):
-    colored_mask = np.zeros(
-        (config["image_height"], config["image_width"], 3), dtype=np.uint8
-    )
-    for class_id in range(21):
-        class_color = np.random.randint(0, 255, size=3)
-        colored_mask[frame[:, :, class_id] == 1] = class_color
-        return colored_mask
+def colorize_seg_image(original, segmented):
+    segmented_color = np.zeros_like(original)
+    for class_idx in range(segmented.shape[-1]):
+        mask = segmented[:, :, class_idx] == 1
+        segmented_color[mask] = color_mapping[class_idx]
+    return segmented_color
 
 
 def predict_with_model(model, image):
@@ -46,12 +68,19 @@ def process_video(video_path):
         if not ret:
             break
         frame_raw = cv2.resize(
-            frame_raw, (config["image_height"], config["image_width"])
+            frame_raw, (config["image_width"], config["image_height"])
         )
         frame_out = predict_with_model(model, frame_raw)
-        frame_color = colorize_seg_image(frame_out)
-
-        stacked_frame = np.hstack((frame_raw, frame_color))
+        frame_out = cv2.normalize(
+            frame_out,
+            None,
+            alpha=0,
+            beta=255,
+            norm_type=cv2.NORM_MINMAX,
+            dtype=cv2.CV_32F,
+        ).astype(np.uint8)
+        frame_out = colorize_seg_image(frame_raw, frame_out)
+        stacked_frame = np.hstack((frame_raw, frame_out))
         cv2.imshow("Original Vs Segmented Video", stacked_frame)
         if cv2.waitKey(30) & 0xFF == ord("q"):
             break
