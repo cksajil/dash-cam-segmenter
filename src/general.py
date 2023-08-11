8124541759df6850d399a14290259fb4e327505c
cksajil <sajilck@gmail.com>
2023-08-11 10:22:20 +0530

    create folder creation function

--- src/general.py
+++ src/general.py
@@ -11,3 +11,9 @@ def load_config(config_name):
         config = yaml.safe_load(file)
 
     return config
+
+
+def create_folder(directory):
+    """Function to create a folder in a location if it does not exist"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
