86e9a672d5909c07a2d4381bad0b8988c5cbf521
cksajil <sajilck@gmail.com>
2023-08-11 10:32:24 +0530

    update general functions

--- src/general.py
+++ src/general.py
@@ -1,5 +1,5 @@
 import yaml
-from os import path
+import os
 
 
 def load_config(config_name):
@@ -7,7 +7,7 @@ def load_config(config_name):
     A function to load and return config file in YAML format
     """
     CONFIG_PATH = "./config/"
-    with open(path.join(CONFIG_PATH, config_name)) as file:
+    with open(os.path.join(CONFIG_PATH, config_name)) as file:
         config = yaml.safe_load(file)
 
     return config
